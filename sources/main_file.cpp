0cddba546b02dc83677edb96f33597bb6bc04de4
wangobango <puch003@wp.pl>
2018-06-20 16:54:15 +0200

    123

--- sources/main_file.cpp
+++ sources/main_file.cpp
@@ -163,10 +163,10 @@ void prepareObject(ShaderProgram *shaderProgram) {
 
 //Procedura inicjująca
 void initOpenGLProgram(GLFWwindow* window) {
-	Model model("models/szachy.obj");
+	Model model("models/goniec2.obj");	
 	model.loadArrays(&vertices,&normals,&texCoords);
-	vertexCount = model.vertex_count;
-	std::cout<<vertexCount;
+	std::cout<<model.GetVertexCount();
+	//std::cout<<model.GetVertices.size();
 
 	//************Tutaj umieszczaj kod, który należy wykonać raz, na początku programu************
 	glClearColor(0, 0, 0, 1); //Czyść ekran na czarno
--- sources/obj_parser.cpp
+++ sources/obj_parser.cpp
@@ -18,25 +18,28 @@ Model::Model(const char* path){
                 int res = fscanf(file, "%s", lineHeader);
                 if (res == EOF)
                     break;
+                
                 if ( strcmp( lineHeader, "v" ) == 0 ){
                     glm::vec4 vertex;
                     fscanf(file, "%f %f %f \n", &vertex.x, &vertex.y, &vertex.z );
                     vertex.w = 1.0;
-                    vertex_count++;
+                    IncVertexCount();
                     temp_vertices.push_back(vertex);
                 }
                 else if ( strcmp( lineHeader, "vt" ) == 0 ){
                     glm::vec2 uv;
                     fscanf(file, "%f %f\n", &uv.x, &uv.y );
                     uvs_count++;
-                    out_uvs.push_back(uv);
+                    //out_uvs.push_back(uv);
+                    AddUv(uv);
                 }
                 else if ( strcmp( lineHeader, "vn" ) == 0 ){
                     glm::vec4 normal;
                     fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z );
                     normal.w = 0.0;
                     normals_count++;
-                    out_normals.push_back(normal);
+                    //out_normals.push_back(normal);
+                    AddNormal(normal);
                 }
                 else if ( strcmp( lineHeader, "f" ) == 0 ){
                     std::string vertex1, vertex2, vertex3;
@@ -59,7 +62,8 @@ Model::Model(const char* path){
                 for( unsigned int i=0; i<vertexIndices.size(); i++ ){
                     unsigned int vertexIndex = vertexIndices[i];
                     glm::vec4 vertex = temp_vertices[ vertexIndex-1 ];
-                    out_vertices.push_back(vertex);
+                    //out_vertices.push_back(vertex);
+                    AddVertice(vertex);
                 }
 
         fclose(file);
